1f061ad75a19c02f15d2f1c6be3e85e4637ee09c
Gerardo Amador <gergames11@gmail.com>
2025-11-09 04:54:18 -0600

    modificaciones y migracion en categorias para imagen

---
+++ backend/prisma/migrations/20251109102556_add_column_imagen_in_categories/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Categoria" ADD COLUMN     "imagen" TEXT;
--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -3,15 +3,24 @@ const prisma = new PrismaClient();
 
 async function main() {
   const categorias = [
-    { nombre: "Salud", descripcion: "Servicios relacionados con la salud" },
-    { nombre: "Artesanías", descripcion: "Arte indígena personalizado" },
-    { nombre: "Música", descripcion: "Servicios de mariachi y banda" },
-    { nombre: "Estética", descripcion: "Servicios de estética, sastrería, tejedoras, boleros" },
-    { nombre: "Educación", descripcion: "Cursos y tutorías" },
-    { nombre: "Mano de obra", descripcion: "Servicios de plomería, carpintería, etc." },
-    { nombre: "Legal", descripcion: "Asesoría legal" },
-    { nombre: "Mascotas", descripcion: "Asesoría veterinaria, estética canina, etc." },
+    { nombre: "Salud", descripcion: "Servicios relacionados con la salud", 
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-RatswW7ESQtej99DCRnLup60PHwnxg.png&w=1000&q=75" },
+    { nombre: "Artesanías", descripcion: "Arte indígena personalizado",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-DhMULvtuRLGTsiepvyclJRMosD9UxD.png&w=1000&q=75" },
+    { nombre: "Música", descripcion: "Servicios de mariachi y banda",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-fzkD2FyDowdiTsSYEIr1KCywzu0Q78.png&w=1000&q=75" },
+    { nombre: "Estética", descripcion: "Servicios de estética, sastrería, tejedoras, boleros",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-RCs6a5lnmDpabnkGxGlPRFAZV2yCV3.png&w=1000&q=75" },
+    { nombre: "Educación", descripcion: "Cursos y tutorías",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-fLBUuECujebAYq2Ctir0tmm4ljvF0b.png&w=1000&q=75" },
+    { nombre: "Mano de obra", descripcion: "Servicios de plomería, carpintería, etc.",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-YsAVD6wE8mOeZNdBCJlXyREMXg8yvu.png&w=1000&q=75" },
+    { nombre: "Legal", descripcion: "Asesoría legal",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-tJ1togoNqunAE3CBiqtQlFvXCiBxJp.png&w=1000&q=75" },
+    { nombre: "Mascotas", descripcion: "Asesoría veterinaria, estética canina, etc.",
+      imagen: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-lOgGbuHbg7HhqroCWguLxuxeMazgSQ.png&w=1000&q=75" },
   ];
+  
   const usuarios = [
     { nombre: "Daniel Mondragon", email: "daniel@prueba.com", 
       password: "$2b$10$XO123BumQJvpSW00gpdDLuvTVV66ZQBnGBbpMog38ft3y4WCauOhO",
